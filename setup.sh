422bea0e4da0cf10fd69d171af95623111ea91d4
W1llyu <will.yu@risewinter.com>
2018-01-30 11:27:16 +0800

    [UPDATE] 修改打包shell

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 path="target"
 mvn clean install -U
 rm -rf $path
-cp -rf ./out $path
-cp -rf ./tidyspider-core/target/lib $path/libs
+cp -rf ./shell $path
+cp -rf ./conf $path/conf
+cp -rf ./tidyspider-lib/target/lib $path/libs
 cp -f ./tidyspider-core/target/tidyspider-core-0.1.0.jar $path/tidyspider-core.jar
 cp -f ./tidyspider-samples/target/tidyspider-samples-0.1.0.jar $path/tidyspider-samples.jar
 rm -f tidyspider.zip
